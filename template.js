d6bf216476dad4eeb3d1ed3ad5bfe6925939e15d
Sergei <sergnevo@rambler.ru>
2022-01-20 18:24:52 +0300

    first commit

---
+++ template.js
@@ -0,0 +1,12 @@
+export default () => {
+  return `<!doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>My Fullstack</title>
+        </head>
+        <body>
+            <div id="root">Дипломная работа</div>
+        </body>
+    </html>`
+}
