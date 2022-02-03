b946209d52d3f460c92716f621ebd31d0474793c
Sergei <sergnevo@rambler.ru>
2022-02-03 12:36:51 +0300

    config

---
+++ config/production.js
@@ -0,0 +1,7 @@
+const config = {
+  env: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 5001,
+  jwtSecret: process.env.JWT_SECRET || "Neo_secret_key",
+  mongoUri: "mongodb+srv://neo:diplom@cluster0.d7huc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+}
+export default config
